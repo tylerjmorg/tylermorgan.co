7688b0a7b7fd824480182846f7b352d6af6156ca
Tyler Morgan <107895555+TylerJMorg@users.noreply.github.com>
2024-05-19 02:20:34 -0600

    Accessibility improvements

--- ariaCollapsibleMod.js
+++ ariaCollapsibleMod.js
@@ -1,14 +1,21 @@
 function toggleAriaValues() {
-    var button = document.getElementById("aboutMeButton");
-    var element = document.getElementById("aboutMeContent");
-  
-    if (button.getAttribute("aria-expanded") === "true") {
-      button.setAttribute("aria-expanded", "false");
-      element.setAttribute("aria-hidden", "true");
-    } else {
-      button.setAttribute("aria-expanded", "true");
-      element.setAttribute("aria-hidden", "false");
-    }
+  var button = document.getElementById("aboutMeButton");
+  var element = document.getElementById("aboutMeContent");
+  var contentLinks = element.querySelectorAll(".content-link, .dark-btn");
+
+  if (button.getAttribute("aria-expanded") === "true") {
+    button.setAttribute("aria-expanded", "false");
+    element.setAttribute("aria-hidden", "true");
+    contentLinks.forEach(function(link) {
+      link.setAttribute("tabindex", "-1");
+    });
+  } else {
+    button.setAttribute("aria-expanded", "true");
+    element.setAttribute("aria-hidden", "false");
+    contentLinks.forEach(function(link) {
+      link.setAttribute("tabindex", "0");
+    });
   }
-  
-  document.getElementById("aboutMeButton").addEventListener("click", toggleAriaValues);
+}
+
+document.getElementById("aboutMeButton").addEventListener("click", toggleAriaValues);
