31af118b823add49ede13af7c5dbcd7f06151aa8
Tyler Morgan <107895555+TylerJMorg@users.noreply.github.com>
2024-06-01 08:26:14 -0600

    Test using AVIF favicons

--- collapsible.js
+++ collapsible.js
@@ -1,11 +1,11 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// The actual function.
+let coll = document.getElementsByClassName("collapsible");
 
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var targetId = this.getAttribute("data-target-id");
-    var content = document.getElementById(targetId);
+    let targetId = this.getAttribute("data-target-id");
+    let content = document.getElementById(targetId);
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
@@ -13,3 +13,26 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+// Accessible for screen readers and tab indexing.
+function toggleAriaValues() {
+  let button = document.getElementById("aboutMeButton");
+  let element = document.getElementById("aboutMeContent");
+  let contentLinks = element.querySelectorAll(".content-link, .dark-btn");
+
+  if (button.getAttribute("aria-expanded") === "true") {
+    button.setAttribute("aria-expanded", "false");
+    element.setAttribute("aria-hidden", "true");
+    contentLinks.forEach(function(link) {
+      link.setAttribute("tabindex", "-1");
+    });
+  } else {
+    button.setAttribute("aria-expanded", "true");
+    element.setAttribute("aria-hidden", "false");
+    contentLinks.forEach(function(link) {
+      link.setAttribute("tabindex", "0");
+    });
+  }
+}
+
+document.getElementById("aboutMeButton").addEventListener("click", toggleAriaValues);
