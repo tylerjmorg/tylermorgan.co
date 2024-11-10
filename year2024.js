a7e14fbfe0f5b13cfdeee40b3c9804ccdc10c76f
Tyler Morgan <git@tylerjm.org>
2024-11-10 09:59:58 -0700

    Update year2024.js

--- year2024.js
+++ year2024.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://tylermorgan.co/currentyear.json')
+  fetch(`${window.location.origin}/currentyear.json`)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
