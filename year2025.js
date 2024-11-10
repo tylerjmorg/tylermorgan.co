cf97cde177b206948a4a028e03593560608f5a30
Tyler Morgan <git@tylerjm.org>
2024-11-09 19:04:04 -0700

    minor changes

---
+++ year2025.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://tylermorgan.co/currentyear.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const currentYearElement = document.getElementById('year2025');
+      const year = data.year;
+      if (year <= 2025) {
+        currentYearElement.textContent = '2025';
+      } else {
+        currentYearElement.textContent = `2025–${year}`;
+      }
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+});
