9790843aca03c032cae31d2055e846befc621c55
Tyler Morgan <git@tylerjm.org>
2025-01-27 21:47:42 -0700

    Update copyright_year.js

--- copyright_year.js
+++ copyright_year.js
@@ -1,5 +1,8 @@
 const currentYear = 2025;
 
+
+const currentYear2 = String(currentYear).slice(-2);
+
 document.addEventListener("DOMContentLoaded", function() {
 
   const copyrightElements = document.querySelectorAll('[data-copyright-year], [data-copyright-endyear], [data-copyright-currentyear]');
@@ -12,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
       element.textContent = currentYear;
     } else if (year && endYear === 'currentyear') {
       element.textContent = `${year}–${currentYear}`;
+    } else if (year && endYear === 'currentyear2') { 
+      element.textContent = `${year}–${currentYear2}`;
     } else if (year && !endYear) {
       element.textContent = year;
     } else if (year && endYear) {
